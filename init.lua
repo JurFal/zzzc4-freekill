7eb17ef5b4663ea1c0d4374ce98e7a0ec3d52b0f
zyc12241252 <zyc12241252@163.com>
2024-02-03 23:39:43 +0800

    test change

--- init.lua
+++ init.lua
@@ -1,7 +1,15 @@
 local extension = Package("zzzc4")
+local zzzc4_pao = require "packages/zzzc4/zzzc4_pao"
+local zzzc4_xue = require "packages/zzzc4/zzzc4_xue"
+
 
 Fk:loadTranslationTable{
-  ["zzzc4"] = "班杀",
+    ["zzzc4"] = "班杀",
+  }
+
+return {
+    extension,
+    zzzc4_pao,
+    zzzc4_xue,
 }
 
-return { extension }

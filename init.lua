8a66cac2c61a41dedd792c7de06758a208642cf2
zyc12241252 <zyc12241252@163.com>
2024-07-22 17:20:49 +0800

    test

--- init.lua
+++ init.lua
@@ -1,15 +1,165 @@
 local extension = Package("zzzc4")
-local zzzc4_pao = require "packages/zzzc4/zzzc4_pao"
-local zzzc4_xue = require "packages/zzzc4/zzzc4_xue"
+--[[local zzzc4_pao = require "packages/zzzc4/zzzc4_pao"
+local zzzc4_xue = require "packages/zzzc4/zzzc4_xue"]]
 
 
 Fk:loadTranslationTable{
     ["zzzc4"] = "班杀",
   }
 
+
+
+local zzz_youya = General(extension, "zzz_youya", "zzz", 3, 3, General.Female)
+--[[
+local biyue = fk.CreateTriggerSkill{
+  name = "biyue",
+  anim_type = "drawcard",
+  events = {fk.EventPhaseStart},
+  can_trigger = function(self, event, target, player, data)
+    return target == player and player:hasSkill(self)
+      and player.phase == Player.Finish
+  end,
+  on_use = function(self, event, target, player, data)
+    player:drawCards(1, self.name)
+  end,
+}
+local bingzheng = fk.CreateTriggerSkill{
+  name = "bingzheng",
+  anim_type = "control",
+  events = {fk.EventPhaseEnd},
+  can_trigger = function(self, event, target, player, data)
+    return target == player and player:hasSkill(self) and player.phase == Player.Play and
+      not table.every(player.room:getAlivePlayers(), function(p) return p:getHandcardNum() == p.hp end)
+  end,
+  on_cost = function(self, event, target, player, data)
+    local to = player.room:askForChoosePlayers(player, table.map(table.filter(player.room:getAlivePlayers(), function(p)
+      return p:getHandcardNum() ~= p.hp end), Util.IdMapper), 1, 1, "#bingzheng-choose", self.name, true)
+    if #to > 0 then
+      self.cost_data = to[1]
+      return true
+    end
+  end,
+  on_use = function(self, event, target, player, data)
+    local room = player.room
+    local to = room:getPlayerById(self.cost_data)
+    local choices = {"bingzheng_draw"}
+    if not to:isKongcheng() then
+      table.insert(choices, 1, "bingzheng_discard")
+    end
+    local choice = room:askForChoice(player, choices, self.name, "#bingzheng-choice::"..to.id)
+    if choice == "bingzheng_draw" then
+      to:drawCards(1, self.name)
+    else
+      room:askForDiscard(to, 1, 1, false, self.name, false)
+    end
+    if #to.player_cards[Player.Hand] == to.hp then
+      player:drawCards(1, self.name)
+      if to ~= player then
+        local card = room:askForCard(player, 1, 1, true, self.name, true, ".", "#bingzheng-card::"..to.id)
+        if #card > 0 then
+          room:obtainCard(to, card[1], false, fk.ReasonGive)
+        end
+      end
+    end
+  end,
+}
+local ty__langmie = fk.CreateTriggerSkill{
+  name = "ty__langmie",
+  mute = true,
+  events = {fk.EventPhaseEnd, fk.EventPhaseStart},
+  can_trigger = function(self, event, target, player, data)
+    if player:hasSkill(self) and target ~= player then
+      if event == fk.EventPhaseEnd and target.phase == Player.Play then
+        local count = {0, 0, 0}
+        player.room.logic:getEventsOfScope(GameEvent.UseCard, 999, function(e)
+          local use = e.data[1]
+          if use.from == target.id then
+            if use.card.type == Card.TypeBasic then
+              count[1] = count[1] + 1
+            elseif use.card.type == Card.TypeTrick then
+              count[2] = count[2] + 1
+            elseif use.card.type == Card.TypeEquip then
+              count[3] = count[3] + 1
+            end
+          end
+        end, Player.HistoryPhase)
+        return table.find(count, function(i) return i > 1 end)
+      elseif event == fk.EventPhaseStart and target.phase == Player.Finish and not player:isNude() then
+        local n = 0
+        player.room.logic:getEventsOfScope(GameEvent.ChangeHp, 999, function(e)
+          local damage = e.data[5]
+          if damage and target == damage.from then
+            n = n + damage.damage
+          end
+        end, Player.HistoryTurn)
+        return n > 1
+      end
+    end
+  end,
+  on_cost = function (self, event, target, player, data)
+    if event == fk.EventPhaseEnd then
+      return player.room:askForSkillInvoke(player, self.name, nil, "#ty__langmie-draw")
+    elseif event == fk.EventPhaseStart then
+      local card = player.room:askForDiscard(player, 1, 1, true, self.name, true, ".", "#ty__langmie-damage::"..target.id, true)
+      if #card > 0 then
+        self.cost_data = card
+        return true
+      end
+    end
+  end,
+  on_use = function(self, event, target, player, data)
+    local room = player.room
+    player:broadcastSkillInvoke(self.name)
+    if event == fk.EventPhaseEnd then
+      room:notifySkillInvoked(player, self.name, "drawcard")
+      player:drawCards(1, self.name)
+    elseif event == fk.EventPhaseStart then
+      room:notifySkillInvoked(player, self.name, "offensive")
+      room:doIndicate(player.id, {target.id})
+      room:throwCard(self.cost_data, self.name, player, player)
+      room:damage{
+        from = player,
+        to = target,
+        damage = 1,
+        skillName = self.name,
+      }
+    end
+  end,
+}
+]]
+local zzz_yyouya = fk.CreateTriggerSkill{
+    name = "zzz_yyouya",
+    events = {fk.EventPhaseStart},
+    anim_type = "drawcard",
+    can_trigger = function(self, event, target, player, data)
+      return target.phase == Player.Finish and player:hasSkill(self) and player:getHandcardNum() ~= player.hp
+    end,
+    on_cost = function(self, event, target, player, data)
+      return player:hasSkill(self) and player.room:askForSkillInvoke(player, self.name, nil, "#zzz_yyouya-invoke")
+    end,
+    on_trigger = function(self, event, target, player, data)
+      local room = player.room
+      if not player:hasSkill(self) then return end
+      if player:getHandcardNum() < player.hp then
+        player:drawCards(1, self.name)
+      elseif player:getHandcardNum() > player.hp and player:isWounded() then
+        room:recover{
+          who = player,
+          num = 1,
+          recoverBy = player,
+          skillName = self.name
+        }
+      end
+    end
+}
+  
+zzz_youya:addSkill(zzz_yyouya)
+zzz_youya:addSkill("songci")
+ 
+
 return {
     extension,
-    zzzc4_pao,
-    zzzc4_xue,
+    --[[zzzc4_pao,
+    zzzc4_xue,]]
 }
 
